2516740bf7aa2ee200b0b741f09333f1a1835f08
urchincolley <urchin.colley@gmail.com>
2015-11-13 12:05:01 -0800

    removed bad brackets

--- RanDominion.js
+++ RanDominion.js
@@ -1,9 +1,7 @@
 var rdApp = angular.module('rdApp', ['checklist-model']);
 
-  rdApp.filter('notInArray', function() {
-    return function(array, value) {
-      return array.indexOf(value) < 0;
-    };
-  });
-
-}]);
+rdApp.filter('notInArray', function() {
+  return function(array, value) {
+    return array.indexOf(value) < 0;
+  };
+});
